9931c4005c25ff197241424e048594b405caf7a4
Vishesh Kumar <131744489+vishesh551000@users.noreply.github.com>
2024-08-21 15:21:16 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,26 @@
+let score = 44
+
+// console.log(typeof score);
+ // console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// "33" =>33
+//  "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "vishesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false;
+//"" =>false
+//"vishesh" => true;
+
+let someNumber = 32
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
